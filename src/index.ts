3461c6344bcc7e6b3973e8ac9194c616e0c071ab
Adam Stover <nehtaro@gmail.com>
2020-07-19 19:58:02 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export function tryNice<
   F extends (...arg: Array<Argument<F>>) => V | undefined = any
 >(fn: F, ...args: Array<Argument<F>>): [V?, E?] {
   try {
-    return [fn.apply(null, args)]
+    return [Reflect.apply(fn, null, args)]
   } catch (error) {
     return [undefined, error as E]
   }
@@ -40,7 +40,61 @@ export async function tryNiceAsync<
   F extends (...arg: Array<Argument<F>>) => V | undefined = any
 >(fn: F, ...args: Array<Argument<F>>): Promise<[V?, E?]> {
   try {
-    return [await fn.apply(null, args)]
+    return [Reflect.apply(fn, null, args)]
+  } catch (error) {
+    return [undefined, error as E]
+  }
+}
+
+
+/**
+ * Clean try/catch wrapper with 'this' as an argument
+ *
+ * @export
+ * @template V Return value
+ * @template E Error
+ * @template T This
+ * @template F Function
+ * @param {F} fn
+ * @param {T} thisArg
+ * @param {...Array<Argument<F>>} args
+ * @returns {[V?, E?]}
+ */
+export function tryThis<
+  V = any,
+  E = Error,
+  F extends (...arg: Array<Argument<F>>) => V | undefined = any,
+  T = any
+>(fn: F, thisArg: T, ...args: Array<Argument<F>>): [V?, E?] {
+  try {
+    return [Reflect.apply(fn, thisArg, args)]
+  } catch (error) {
+    return [undefined, error as E]
+  }
+}
+
+
+/**
+ * Clean async try/catch wrapper with 'this' as an argument
+ *
+ * @export
+ * @template V Return value
+ * @template E Error
+ * @template T This
+ * @template F Function
+ * @param {F} fn
+ * @param {T} thisArg
+ * @param {...Array<Argument<F>>} args
+ * @returns {Promise<[V?, E?]>}
+ */
+export async function tryThisAsync<
+  V = any,
+  E = Error,
+  F extends (...arg: Array<Argument<F>>) => V | undefined = any,
+  T = any
+>(fn: F, thisArg: T, ...args: Array<Argument<F>>): Promise<[V?, E?]> {
+  try {
+    return [await Reflect.apply(fn, thisArg, args)]
   } catch (error) {
     return [undefined, error as E]
   }
